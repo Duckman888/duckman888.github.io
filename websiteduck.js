07812bdcd02f31c687ea4cb012b2f3b0d5ec5abc
Duckman888 <andreas.holmbakken@gmail.com>
2021-04-21 16:17:03 +0200

    Add files via upload

---
+++ websiteduck.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+x.classList.toggle("change");
+} 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
